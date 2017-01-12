0858661a498a7a409ab1adc0615380a9fcc472b2
Balaji Venkatesh <balajiv@chimeratechnologies.com>
2017-01-12 13:23:43 +0530

    add additional files

---
+++ gulpfile.js
@@ -0,0 +1,142 @@
+'use strict';
+
+var gulp            = require('gulp');
+var plugins         = require('gulp-load-plugins')();
+var _               = require('lodash');
+var browserify      = require('browserify');
+var source          = require('vinyl-source-stream');
+var buffer          = require('vinyl-buffer');
+var fs              = require('fs');
+var argv            = require('yargs').argv;
+var path            = require('path');
+var os              = require('os');
+
+var v;
+function version () {
+  var previous = require('./package.json').version;
+  if (!v) v = require('semver').inc(previous, argv.type || 'patch');
+  return v;
+}
+
+function bundle () {
+  var pkg = require('./package.json');
+  return browserify({
+      standalone: 'firebasemock'
+    })
+    .add(pkg.main)
+    .bundle()
+    .pipe(source('firebasemock.js'))
+    .pipe(buffer())
+    .pipe(plugins.header(fs.readFileSync('./helpers/header.txt'), {
+      pkg: _.extend(require('./package.json'), {
+        version: version()
+      })
+    }))
+    .pipe(plugins.footer(fs.readFileSync('./helpers/globals.js')));
+}
+
+var bundlePath;
+gulp.task('bundle-smoke', function () {
+  var name = Date.now() + '-firebasemock.js';
+  var dir = os.tmpdir();
+  bundlePath = path.join(dir, name);
+  return bundle()
+    .pipe(plugins.rename(name))
+    .pipe(gulp.dest(dir));
+});
+
+gulp.task('bundle', function () {
+  return bundle().pipe(gulp.dest('./browser'));
+});
+
+gulp.task('cover', function () {
+  return gulp.src(['./src/**/*.js', '!./src/login.js'])
+    .pipe(plugins.istanbul())
+    .pipe(plugins.istanbul.hookRequire());
+});
+
+gulp.task('test', ['cover'], function () {
+  return gulp.src('test/unit/*.js')
+    .pipe(plugins.mocha({
+      grep: argv.grep
+    }))
+    .pipe(plugins.istanbul.writeReports());
+});
+
+gulp.task('karma', function () {
+  return require('karma-as-promised').server.start({
+    frameworks: ['browserify', 'mocha', 'sinon'],
+    browsers: ['PhantomJS'],
+    client: {
+      args: ['--grep', argv.grep]
+    },
+    files: [
+      'node_modules/es5-shim/es5-shim.js',
+      'test/unit/*.js'
+    ],
+    plugins: [
+      'karma-phantomjs-launcher',
+      'karma-mocha',
+      'karma-sinon',
+      'karma-browserify'
+    ],
+    preprocessors: {
+      'test/unit/*.js': ['browserify']
+    },
+    browserify: {
+      debug: true
+    },
+    autoWatch: false,
+    singleRun: true
+  });
+});
+
+gulp.task('smoke', ['bundle-smoke'], function () {
+  return require('karma-as-promised').server.start({
+    frameworks: ['mocha', 'chai'],
+    browsers: ['PhantomJS'],
+    client: {
+      args: ['--grep', argv.grep]
+    },
+    files: [
+      bundlePath,
+      'test/smoke/globals.js'
+    ],
+    plugins: [
+      'karma-phantomjs-launcher',
+      'karma-mocha',
+      'karma-chai'
+    ],
+    autoWatch: false,
+    singleRun: true
+  });
+});
+
+gulp.task('lint', function () {
+  return gulp.src(['./gulpfile.js', './src/**/*.js', './test/**/*.js'])
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'))
+    .pipe(plugins.jshint.reporter('fail'));
+});
+
+var pkgs = ['./package.json', './bower.json'];
+gulp.task('bump', function () {
+  return gulp.src(pkgs)
+    .pipe(plugins.bump({
+      version: version()
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('release', ['bundle', 'bump'], function () {
+  var versionString = 'v' + version();
+  var message = 'Release ' + versionString;
+  return plugins.shell.task([
+    'git add -f ./browser/firebasemock.js',
+    'git add ' + pkgs.join(' '),
+    'git commit -m "' + message + '"',
+    'git tag ' + versionString,
+    'git push',
+    'git push --tags'
+  ])();
+});
---
+++ helpers/globals.js
@@ -0,0 +1,67 @@
+;(function (window) {
+  'use strict';
+  if (typeof window !== 'undefined' && window.firebasemock) {
+    window.MockFirebase = window.firebasemock.MockFirebase;
+    window.MockFirebaseSimpleLogin = window.firebasemock.MockFirebaseSimpleLogin;
+
+    window.firebasemock.MockFirebaseSdk = {
+      database: function() {
+        return {
+          ref: function(path) {
+            return new window.firebasemock.MockFirebase(path);
+          },
+          refFromURL: function(url) {
+            return new window.firebasemock.MockFirebase(url);
+          }
+        };
+      },
+      auth: function() {
+        var auth = new window.firebasemock.MockFirebase();
+        delete auth.ref;
+        return auth;
+      },
+      initializeApp: function(options){
+        return {
+          {
+            auth: function(){
+              return window.firebasemock.MockFirebaseSdk.auth;
+            },
+            database: function(){
+              return window.firebasemock.MockFirebaseSdk.database;
+            }
+          }
+        }
+      }
+    };
+    window.firebasemock.MockFirebaseSdk.auth.GoogleAuthProvider = function() {
+      this.providerId = "google.com";
+    };
+    window.firebasemock.MockFirebaseSdk.auth.TwitterAuthProvider = function() {
+      this.providerId = "twitter.com";
+    };
+    window.firebasemock.MockFirebaseSdk.auth.FacebookAuthProvider = function() {
+      this.providerId = "facebook.com";
+    };
+    window.firebasemock.MockFirebaseSdk.auth.GithubAuthProvider = function() {
+      this.providerId = "github.com";
+    };
+
+    var originals = false;
+    window.MockFirebase.override = function () {
+      originals = {
+        firebasesdk: window.firebase,
+        firebase: window.Firebase,
+        login: window.FirebaseSimpleLogin
+      };
+      window.firebase = window.firebasemock.MockFirebaseSdk;
+      window.Firebase = window.firebasemock.MockFirebase;
+      window.FirebaseSimpleLogin = window.firebasemock.MockFirebaseSimpleLogin;
+    };
+    window.MockFirebase.restore = function () {
+      if (!originals) return;
+      window.firebase = originals.firebasesdk;
+      window.Firebase = originals.firebase;
+      window.FirebaseSimpleLogin = originals.login;
+    };
+  }
+})(window);
